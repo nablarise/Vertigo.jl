13f2f0f76f1e1c66b64e2c26b80f454712c942e2
Guillaume Marques <guillaume.marques@protonmail.com>
2026-03-20 23:06:15 +0100

    test: remove trivial getter/setter tests for max_cg_iterations

--- test/VertigoUnitTests/colgen/test_max_cg_iterations.jl
+++ test/VertigoUnitTests/colgen/test_max_cg_iterations.jl
@@ -2,30 +2,11 @@
 # Author: Guillaume Marques <guillaume@nablarise.com>
 # SPDX-License-Identifier: Proprietary
 
-using Vertigo.ColGen: ColGenContext, ColGenLoggerContext, max_cg_iterations,
+using Vertigo.ColGen: ColGenLoggerContext, max_cg_iterations,
     set_max_cg_iterations!, stop_colgen_phase,
     ColGenIterationOutput, Phase0, Phase1, Phase2
 
 function test_max_cg_iterations()
-    @testset "[max_cg_iterations] default value" begin
-        inst = random_gap_instance(2, 5)
-        ctx = build_gap_context(inst)
-        @test max_cg_iterations(ctx) == 1000
-    end
-
-    @testset "[max_cg_iterations] set and get" begin
-        inst = random_gap_instance(2, 5)
-        ctx = build_gap_context(inst)
-        set_max_cg_iterations!(ctx, 10)
-        @test max_cg_iterations(ctx) == 10
-    end
-
-    @testset "[max_cg_iterations] constructor kwarg" begin
-        inst = random_gap_instance(2, 5)
-        ctx = build_gap_context(inst; max_cg_iterations=50)
-        @test max_cg_iterations(ctx) == 50
-    end
-
     @testset "[max_cg_iterations] stop_colgen_phase respects limit" begin
         inst = random_gap_instance(2, 5)
         ctx = build_gap_context(inst; max_cg_iterations=5)
